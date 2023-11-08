c9c38b94a704e5f6c38fa151ea6751305d63b168
naruto <naruto@commander.naruto>
2023-11-08 13:12:05 +0530

    Array Dstructing All Conspect Here

--- destructingArrays/main.js
+++ destructingArrays/main.js
@@ -1,28 +1,52 @@
-//..........................................................Destructing with arrays................................................................
+const restaurant = {
+  name: "Classico Italiano",
 
-//this my family but some another pepole here now go extract that pepoles
-let myFamily = [
-  "murugesan",
-  "vijayakodi",
-  "santhosh",
-  "ajay",
-  "harish",
-  "nadiya",
-];
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
 
-let [, , , , anotherMan, anotherGirl] = myFamily;
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
 
-console.log(
-  `This guy ${anotherMan}, and This girl ${anotherGirl} not on my Family`
-);
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
 
-// this type def using ...restOperator getting a balance value
-const [dad, mom, bigBrother, sumallBrother, ...restInpeace] = myFamily;
-console.log(dad, mom, bigBrother, sumallBrother, restInpeace);
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
-//swab to mom dad place
-let [main, sub] = myFamily;
+  orders: function (starterMenuIndex, mainMenuIndex) {
+    return [
+      `This was a startMenu List  ${this.categories[starterMenuIndex]} And then Thos All was a ${this.starterMenu[mainMenuIndex]}`,
+    ];
+  },
 
-[main, sub] = [sub, main];
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
 
-console.log(main, sub);
+//Arrays destucting
+const [firstElement, secoundElemnt, thirdElement] = restaurant.categories;
+console.log(firstElement, secoundElemnt, thirdElement);
+
+//Nested Array destucting
+let new_Single_Aarry = ["santhosh", "ajay", ["murugesan", "vijaya"]];
+const [me, , [myDad, myMom]] = new_Single_Aarry;
+console.log(me, myDad, myMom);
+
+//using function with destructing
+console.log(...restaurant.orders(2, 1));
+const [oredr1, ...[order2]] = restaurant.orders(3, 3);
+console.log(oredr1, order2);
+
+//swabing in array destrcting
+
+let [swab1, swab2] = restaurant.mainMenu;
+[swab2, swab1] = [swab1, swab2];
+console.log(swab1, swab2);
